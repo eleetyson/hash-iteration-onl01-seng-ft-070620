9b12f41f962620e26b56f3a2001a4ec05256205d
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-08 13:44:50 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ end
 
 def age_appropriate_birthday()
   birthday_kids.each |name, age|
-    if 
+    if age <= 12
       puts "Happy Birthday #{name}! You are now #{age} years old!"
     end
   end
